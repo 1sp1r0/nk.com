bdff761fc4033f10c693260fa3c6884176f3f6c1
Josh Mobley <josh@newkind.com>
2016-01-22 17:30:57 -0500

    tweaking workshops landing

--- wp-content/themes/milton/includes/eventbrite/eb_workshops_landing_output.php
+++ wp-content/themes/milton/includes/eventbrite/eb_workshops_landing_output.php
@@ -4,7 +4,8 @@ $startTime =  gmdate('D / M j<\s\p\a\n>g:i', strtotime($event->start->local));
 $endTime =  gmdate('g:i <\b>a', strtotime($event->end->local));
 
 echo '<div class="workshop">';
-echo '<a href="'.get_the_permalink().'"><h4 style="background:url('.$event->logo->url.') top center no-repeat;">'.$event->name->text.'</h4></a>';
+echo '<a href="'.get_the_permalink().'"><h4 style="background:url('.  get_field('workshop_header_bg') . ') top center no-repeat;">'. get_field('workshop_summary') .'</h4></a>';
+echo '<p class="workshop_title">' . get_the_title() . '</p>';
 echo '<p class="workshop_desc">'.get_field('workshop_summary').'</p>';
 echo '<a class="button" href="'.get_the_permalink().'">Learn More &raquo;</a>';
 echo '</a>';
