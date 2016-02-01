17ab615771120c5719df3d9e096699a35d36d33c
Josh Mobley <josh@newkind.com>
2016-02-01 17:04:23 -0500

    tweaking tech landing

--- wp-content/themes/milton/page-technology.php
+++ wp-content/themes/milton/page-technology.php
@@ -5,8 +5,11 @@ get_header();
 
 <div class="new-wrapper">
 
+	<div class="page-intro-image">
+	</div>
+
 	
-	<section class="bg-blue">
+	<section class="bg-blue intro">
 		
 		<div class="wrapper">
 		
@@ -19,43 +22,41 @@ get_header();
 	<section>
 		
 		<div class="wrapper">
+
+			<div class="align-left">
 			
-			<h3><strong>We know what it takes to build a successful technology company because we’ve done it ourselves.</strong></h3>
+				<h3><strong>We know what it takes to build a successful technology company because we’ve done it ourselves.</strong></h3>
 			
-			<p class="red-hat">The founders of New Kind played key roles at Red Hat
-			   as the company—and the broader open source
-			   software movement—revolutionized the technology
-			   industry. Through shared experience leading
-			   communications, brand, and community efforts, we
-			   helped Red Hat grow from one office in Durham, NC
-			   to a global $1 billion industry leader.</p>
+			</div><!--.align-left-->
+
+			<div class="align-right">
+
+				<p class="red-hat">The founders of New Kind played key roles at Red Hat
+				   as the company—and the broader open source
+				   software movement—revolutionized the technology
+				   industry. Through shared experience leading
+				   communications, brand, and community efforts, we
+				   helped Red Hat grow from one office in Durham, NC
+				   to a global $1 billion industry leader.</p>
+
+			</div><!--.align-right-->
 
 
 
 		</div>
 		
 	</section>
-
-	<section class="bg-gray">
-
-		<div class="wrapper">
-
-			<h3><strong>Over the past 6+ years, we’ve helped numerous leading technology companies invent or reinvent their brand and culture using an open, collaborative approach.</strong></h3>
-
-		</div>
-
-	</section>
 	
 	
 	<div class="container">
 
-		<a href="#" class="tile ansible">
+		<a href="#" class="tile ansible image">
 			<span></span>
 			<h4>Ansible</h4>
 			<h5>Brand story, positioning, identity development. Acquired by Red Hat in 2015 for over $100m.</h5>
 		</a>
 
-		<a href="#" class="tile nginx">
+		<a href="#" class="tile nginx image">
 			<span></span>
 			<h4>Nginx</h4>
 			<h5>Brand story, positioning, ongoing  research, design, campaigns.</h5>
@@ -80,12 +81,35 @@ get_header();
 
 	</section>
 
+	<section class="container no-padding">
+
+	<a href="#" class="tile bg-blue">
+
+		<span></span>
+		<h4>Ready to chat?</h4>
+
+		<h5>Lorem ipsum sit amet dolor...</h5>
+
+	</a><!--.tile-->
+
+	<a href="#" class="tile bg-gray">
+		<span></span>
+
+		<h4>Want to dig deeper?</h4>
+
+		<h5>Lorem ipsum sit amet dolor...</h5>
+
+	</a><!--.tile-->
+
+
+</section><!--.container-->
+
 </div>
 
 <?php
 
 
-get_footer();
+get_footer('landing');
 
 
 ?>
