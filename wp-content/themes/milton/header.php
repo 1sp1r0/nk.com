b35182e1b0e0b34790902ab91d16b9d6a137eedc
Josh Mobley <josh@newkind.com>
2016-02-10 18:40:48 -0500

    css cache breaker

--- wp-content/themes/milton/header.php
+++ wp-content/themes/milton/header.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="//cloud.typography.com/70540/772064/css/fonts.css" />
 	<link rel="stylesheet" href="//f.fontdeck.com/s/css/9FRVfkomeCh4H19B/qGVFg41Ywo/<?php echo $_SERVER['SERVER_NAME']; ?>/60449.css" type="text/css" />
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/assets/images/favicon.ico" />
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/screen.css?2.10.2016" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/screen.css?2.10.2016v1.0" />
 	<link rel="stylesheet" type="text/css" media="print" href="<?php bloginfo('template_directory'); ?>/css/print.css" />
 	<meta property="og:title" content="<?php bloginfo('name'); ?><?php wp_title('-'); ?>" />
 	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/social-icons/facebook-og.png" />
@@ -22,7 +22,7 @@
 			include_once(get_template_directory_uri() . '/includes/ga-tracking.php');
 		}
 	?>
-	
+
 </head>
 <body>
 	
