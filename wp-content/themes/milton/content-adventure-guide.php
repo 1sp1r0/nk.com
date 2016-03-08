baeffea0ec3e0e089de43003f9b9cfbf565447ff
Josh Mobley <josh@newkind.com>
2016-03-08 11:53:31 -0500

    adding goal tracking ref to adventure guide

--- wp-content/themes/milton/content-adventure-guide.php
+++ wp-content/themes/milton/content-adventure-guide.php
@@ -1,5 +1,5 @@
 <div class="adventure-guide-callout">
-	<a href="http://info.newkind.com/the-four-critical-building-blocks-of-great-brand-positioning"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/blog/adventure-guide.jpg" /></a>
+	<a href="http://info.newkind.com/the-four-critical-building-blocks-of-great-brand-positioning" data-ref="ag-brandpositioning"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/blog/adventure-guide.jpg" /></a>
 	<h4>The 4 Critical Building Blocks of Great Brand Positioning</h4>
-	<a class="button" href="http://info.newkind.com/the-four-critical-building-blocks-of-great-brand-positioning">Download the Adventure Guide</a>
+	<a class="button" href="http://info.newkind.com/the-four-critical-building-blocks-of-great-brand-positioning" data-ref="ag-brandpositioning">Download the Adventure Guide</a>
 </div>
